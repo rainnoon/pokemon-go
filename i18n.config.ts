5dc5f201b2b69c39cd1995daf1f0d8dc47a78cfb
boy <101509477+rainnoon@users.noreply.github.com>
2025-03-02 16:10:16 +0800

    fix(new): locale type

--- i18n.config.ts
+++ i18n.config.ts
@@ -4,7 +4,7 @@ import { locales } from "./middleware";
 
 export default getRequestConfig(async ({ locale }) => {
   // Validate that the incoming `locale` parameter is valid
-  if (!locales.includes(locale)) notFound();
+  if (!locales.includes(locale as any)) notFound();
 
   return {
     messages: (
